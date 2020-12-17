ce93e79e6f1a668a344d46cd332722ac00e57e95
Arkaraj Ghosh <55324916+Arkaraj@users.noreply.github.com>
2020-12-17 18:14:16 +0530

    New admin search bar

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","Ecom");
+#Local connections
+
+
+#Remote connection
+$con=mysqli_connect("","","","");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Agrotech');
-define('SITE_PATH','http://localhost/Agrotech/');
+define('SITE_PATH','https://agro-tech.herokuapp.com/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'/media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'/media/product/');
